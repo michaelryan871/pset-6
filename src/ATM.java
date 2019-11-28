d1d5dcc3f82ed0d5384d33134b94e42a0e405235
Michael Ryan <michael_ryan@Michaels-Air.fios-router.home>
2019-11-27 22:32:18 -0500

    accountCreation finished.

--- src/ATM.java
+++ src/ATM.java
@@ -8,25 +8,25 @@ public class ATM {
     private Bank bank;
     private User newUser;
 
-    
+
     public static final int VIEW = 1;
     public static final int DEPOSIT = 2;
     public static final int WITHDRAW = 3;
-    public static final int TRANSFER = 4; 
+    public static final int TRANSFER = 4;
     public static final int LOGOUT = 5;
     public static final int FIRST_NAME_WIDTH = 20;
     public static final int LAST_NAME_WIDTH = 30;
-    
-    public static final int INVALID = 0; 
+
+    public static final int INVALID = 0;
     public static final int INSUFFICIENT = 1;
-    public static final int SUCCESS = 2; 
+    public static final int SUCCESS = 2;
     public static final int OVERFLOW = 3;
-    
+
     public ATM() {
         in = new Scanner(System.in);
-        
-        activeAccount = new BankAccount(1234, 123456789, 0, new User("Ryan", "Wilson"));
-        
+
+        // activeAccount = new BankAccount(1234, 123456789, 0, new User("Ryan", "Wilson"));
+
 
         try {
 			this.bank  = new Bank();
@@ -34,30 +34,30 @@ public class ATM {
 			// cleanup any resources (i.e., the Scanner) and exit
 		}
     }
-    
+
     public void startup() {
         long accountNo;
-        String accountString = ""; 
+        String accountString = "";
         int pin;
         System.out.println("Welcome to the AIT ATM!\n");
-        
+
                 while (true) {
                     System.out.print("Account No.: ");
                     accountString = in.nextLine();
                     if (accountString.equals("+")) {
                         accountCreation();
-                    } else {                                 
+                    } else {
                     System.out.print("PIN        : ");
                     pin = in.nextInt();
                     login(Long.parseLong(accountString, 10),pin);
-                }  
-                
+                }
+
           }
-    } 
-  
-    
+    }
+
+
     public void login(long accountNo, int pin) {
-    	if (isValidLogin(accountNo, pin)) {	
+    	if (isValidLogin(accountNo, pin)) {
         	activeAccount = bank.login(accountNo, pin);
             System.out.println("\nHello, again, " + activeAccount.getAccountHolder().getFirstName() + "!");
             boolean validLogin = true;
@@ -79,39 +79,39 @@ public class ATM {
             }
         }
     }
-             
-        
-    
-   
+
+
+
+
     public boolean isValidLogin(long accountNo, int pin) {
-    	boolean valid = true; 
+    	boolean valid = true;
     	try {
-    		valid = bank.login(accountNo, pin) != null ? true : false;  
+    		valid = bank.login(accountNo, pin) != null ? true : false;
     	}catch (Exception e) {
     		valid = false;
     	}
-        return valid;   
+        return valid;
     }
-    
+
     public int getSelection() {
         System.out.println("\n[1] View balance");
         System.out.println("[2] Deposit money");
         System.out.println("[3] Withdraw money");
         System.out.println("[4] Transfer money");
         System.out.println("[5] Logout");
-        
+
         return in.nextInt();
     }
-    
+
     public void showBalance() {
         System.out.println("\nCurrent balance: " + activeAccount.getBalance());
     }
 
-    
+
     public void deposit() {
         System.out.print("\nEnter amount: ");
         double amount = in.nextDouble();
-            
+
         int status = activeAccount.deposit(amount);
         if (status == ATM.INVALID) {
             System.out.println("\nDeposit rejected. Amount must be greater than $0.00.\n");
@@ -123,11 +123,11 @@ public class ATM {
         	System.out.println("\nDeposit accepted.");
         }
     }
-        
+
     public void withdraw() {
         System.out.print("\nEnter amount: ");
         double amount = in.nextDouble();
-            
+
         int status = activeAccount.withdraw(amount);
         if (status == ATM.INVALID) {
             System.out.println("\nWithdrawal rejected. Amount must be greater than $0.00.\n");
@@ -139,7 +139,7 @@ public class ATM {
             System.out.println("\nWithdrawal accepted.");
         }
     }
-    
+
     public void transfer() {
     	long secondedAccountNumber;
     	boolean validAccount = true;
@@ -157,8 +157,8 @@ public class ATM {
         if(bank.getAccount(secondedAccountNumber) == null) {
         	validAccount = false;
         }
-        
-        
+
+
         if(validAccount) {
         	BankAccount transferAccount = bank.getAccount(secondedAccountNumber);
         	int withdrawStatus = activeAccount.withdraw(amount);
@@ -179,66 +179,62 @@ public class ATM {
         }else {
         	System.out.println("\nTransfer rejected. Destination account not found.\n");
         }
-        
-    }
-    
-   
-    public void accountCreation() {    	
-    	System.out.print("\nFirst name: ");
-    	String firstName = in.nextLine(); //can't be null, min = 1, max = 20
 
-    	
-    	
-    	if (firstName == null) {
-    		System.out.println("\nPlease enter your first name.");
-    	} else if (firstName.length() < 1 ) {
-    		System.out.println("\nPlease make sure your first name has more than 0 characters.");
-    	} else if (firstName.length() > 30) {
-    		System.out.println("\nPlease make sure your first name does not exceed 30 characters.");
-    	} else {
-    		System.out.print("Last name: ");
-    		
-    	}
-    	
-    	String lastName = in.nextLine();  //can't be null, min = 1, max = 30
-    	
-    	if (lastName == null) {
-    		System.out.println("\nPlease enter your last name.");
-    	} else if (lastName.length() < 1) {
-    		System.out.println("\nPlease make sure your last name has more than 0 characters");
-    	} else if (lastName.length() > 30) {
-    		System.out.println("\nPlease make sure your last name does not exceed 30 characters.");
-    	} else {
-    		System.out.print("PIN: ");
-    	}
-    	
-    	int pin = in.nextInt(); //between 1000 and 9999
-    	
-    	if (pin > 9999) {
-    		System.out.println("\nPlease make sure your pin does not exceed 9999.");
-    	} else if (pin < 1000) {
-    		System.out.println("\nPlease make sure your pin is larger than 1000.");
-    	} else {
-    		System.out.println("\nThank you. Your  account number is: \n Please login to your newly created account.");
-
-    	}
     }
- 
-    
-    
-    
-    public void shutdown() {    	
+
+
+    public void accountCreation() {
+        System.out.print("\nFirst Name: ");
+        String firstName = 	in.nextLine();
+
+        if (firstName.length() <= 20 && firstName.length() > 0) {
+            System.out.print("Last Name: ");
+            String lastName = in.nextLine();
+
+            if (lastName.length() <= 30 && lastName.length() > 0) {
+                System.out.print("Pin: ");
+                if (in.hasNextInt()) {
+                    int pin = in.nextInt();
+                    in.nextLine();
+
+                    if (pin >= 1000 && pin <= 9999) {
+                        newUser = new User(firstName, lastName);
+                        BankAccount newAccount = bank.createAccount(pin, newUser);
+                        System.out.print("\nThank you. Your account number is ");
+                        System.out.println(newAccount.getAccountNo() + ".");
+                        System.out.println("Please login to access your newly created account.");
+                        bank.update(newAccount);
+                        bank.save();
+                    } else {
+                        System.out.println("\nYour pin must be between 1000 and 9999.\n");
+                    }
+                } else {
+                    in.nextLine();
+                    System.out.println("\nYour pin must be numeric.\n");
+                }
+            } else {
+                System.out.println("\nYour last name must be between 1 and 30 characters long.");
+            }
+        } else {
+            System.out.println("\nYour first name must be between 1 and 20 characters long.");
+        }
+    }
+
+
+
+
+    public void shutdown() {
         if (in != null) {
             in.close();
         }
-        
+
         System.out.println("\nGoodbye!");
         System.exit(0);
     }
-    
+
     public static void main(String[] args) {
         ATM atm = new ATM();
-        
+
         atm.startup();
     }
 }
